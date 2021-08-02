83dacb6766f20783da10b8169342c2c8f3c75da0
gaiveknopf <avigaive@hotmail.com>
2021-08-02 08:19:48 -0300

    test: ensure RemoteAuthentication returns an account on 200

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -28,6 +28,16 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
+    when(
+      () => httpClient!.request(
+          method: any(named: 'method'),
+          url: any(named: 'url'),
+          body: any(named: 'body')),
+    ).thenAnswer(
+      (_) async =>
+          {'accessToken': faker.guid.guid(), 'name': faker.person.name()},
+    );
+
     await sut!.auth(params!);
     verify(() => httpClient!.request(
         url: url!,
@@ -77,5 +87,20 @@ void main() {
 
     final future = sut!.auth(params!);
     expect(future, throwsA(DomainError.invalidCredentials));
+  });
+
+  test('Should return an account if HttpClient returns 200', () async {
+    final accessToken = faker.guid.guid();
+    when(
+      () => httpClient!.request(
+          method: any(named: 'method'),
+          url: any(named: 'url'),
+          body: any(named: 'body')),
+    ).thenAnswer(
+      (_) async => {'accessToken': accessToken, 'name': faker.person.name()},
+    );
+
+    final account = await sut!.auth(params!);
+    expect(account.token, accessToken);
   });
 }

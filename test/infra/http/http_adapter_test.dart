e21c76c351b79a2fb2606c2824f789e031a6af2e
gaiveknopf <avigaive@hotmail.com>
2021-08-02 14:42:43 -0300

    last commit

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
+
+class HttpAdapter {
+  final Client client;
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    required String url,
+    required String method,
+    Map? body,
+  }) async {
+    final headers = {
+      'Content-Type': 'application/json',
+      'accept': 'application/json',
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(
+      Uri.parse(url),
+      headers: headers,
+      body: jsonBody,
+    );
+  }
+}
+
+class ClientSpy extends Mock implements Client {}
+
+void main() {
+  HttpAdapter? sut;
+  ClientSpy? client;
+  String url = faker.internet.httpUrl();
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client!);
+    url = faker.internet.httpUrl();
+  });
+
+  group('POST', () {
+    test('Should call post with correct values', () async {
+      await sut!
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+
+      verify(() => client!.post(
+            Uri.parse(url),
+            headers: {
+              'Content-Type': 'application/json',
+              'accept': 'application/json',
+            },
+            body: '{"any_key":"any_value"}',
+          ));
+    });
+
+    test('Should call post without body', () async {
+      await sut!.request(url: url, method: 'post');
+
+      verify(() => client!.post(any(), headers: any(named: 'headers')));
+    });
+  });
+}

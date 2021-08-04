3d0e70097292bb392132c11d1c073addda8f4e25
gaiveknopf <avigaive@hotmail.com>
2021-08-04 20:34:42 -0300

    ensure LoginPage hide loading

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -57,6 +57,7 @@ void main() {
 
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
     expect(button.onPressed, null);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 
   testWidgets('Should call Validate with correct values',
@@ -185,5 +186,16 @@ void main() {
     await tester.pump();
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should hide loading', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    isLoadingController.add(false);
+    await tester.pump();
+
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }

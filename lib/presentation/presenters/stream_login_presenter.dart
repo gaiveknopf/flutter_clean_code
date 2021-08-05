0d555e4a8dedc193380c579df305e0b3f4ae78ac
gaiveknopf <avigaive@hotmail.com>
2021-08-05 10:57:28 -0300

    ensure StreamLoginPresenter calls Validation on password changed

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -5,6 +5,7 @@ import '../protocols/protocols.dart';
 
 class LoginState {
   String emailError;
+  String passwordError;
   bool get isFormValid => false;
 }
 
@@ -25,5 +26,11 @@ class StreamLoginPresenter {
   void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
     _controller.add(_state);
+  }
+
+  void validatePassword(String password) {
+    // _state.passwordError =
+    validation.validate(field: 'password', value: password);
+    // _controller.add(_state);
   }
 }

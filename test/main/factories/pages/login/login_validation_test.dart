4f5715ee826d15b579bf01d0f8e0cbd130174088
gaiveknopf <avigaive@hotmail.com>
2021-08-11 14:28:05 -0300

    ensure LoginValidationFactory return correct validations

---
+++ test/main/factories/pages/login/login_validation_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+import 'package:flutter_app/validation/validators/validators.dart';
+import 'package:flutter_app/main/factories/factories.dart';
+
+void main() {
+  test('Should return the correct validations', () {
+    final validations = makeLoginValidations();
+
+    expect(validations, [
+      RequiredFieldValidation('email'),
+      EmailValidation('email'),
+      RequiredFieldValidation('password'),
+    ]);
+  });
+}

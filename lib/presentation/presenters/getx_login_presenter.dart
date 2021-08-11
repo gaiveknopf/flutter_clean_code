8a7622f1efcad6493a71e356d48717073dea4b9f
gaiveknopf <avigaive@hotmail.com>
2021-08-11 15:41:49 -0300

    add GetxLoginPresenter

---
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -0,0 +1,68 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/state_manager.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/helpers/helpers.dart';
+import '../../domain/usecases/usecases.dart';
+
+import '../protocols/protocols.dart';
+
+import '../../ui/pages/pages.dart';
+
+class GetxLoginPresenter extends GetxController implements LoginPresenter {
+  final Validation validation;
+  final Authentication authentication;
+
+  String _email;
+  String _password;
+  var _emailError = RxString(null);
+  var _passwordError = RxString(null);
+  var _mainError = RxString(null);
+  var _isFormValid = false.obs;
+  var _isLoading = false.obs;
+
+  Stream<String> get emailErrorStream => _emailError.stream;
+  Stream<String> get passwordErrorStream => _passwordError.stream;
+  Stream<String> get mainErrorStream => _mainError.stream;
+  Stream<bool> get isFormValidStream => _isFormValid.stream;
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+
+  GetxLoginPresenter(
+      {@required this.validation, @required this.authentication});
+
+  void validateEmail(String email) {
+    _email = email;
+    _emailError.value = validation.validate(field: 'email', value: email);
+    _validateForm();
+  }
+
+  void validatePassword(String password) {
+    _password = password;
+    _passwordError.value =
+        validation.validate(field: 'password', value: password);
+    _validateForm();
+  }
+
+  void _validateForm() {
+    _isFormValid.value = _emailError.value == null &&
+        _passwordError.value == null &&
+        _email != null &&
+        _password != null;
+  }
+
+  Future<void> auth() async {
+    _isLoading.value = true;
+    _validateForm();
+    try {
+      await authentication
+          .auth(AuthenticationParams(email: _email, password: _password));
+    } on DomainError catch (error) {
+      _mainError.value = error.description;
+    }
+    _isLoading.value = false;
+    _validateForm();
+  }
+
+  // ignore: must_call_super
+  void dispose() {}
+}

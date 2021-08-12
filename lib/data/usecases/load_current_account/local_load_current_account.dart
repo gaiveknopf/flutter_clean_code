1b2deb78d48aa52ed830a647de91976dbeb4a95b
gaiveknopf <avigaive@hotmail.com>
2021-08-12 10:48:52 -0300

    move production code to their own files

---
+++ lib/data/usecases/load_current_account/local_load_current_account.dart
@@ -0,0 +1,22 @@
+import 'package:meta/meta.dart';
+
+import 'package:flutter_app/domain/entities/entities.dart';
+import 'package:flutter_app/domain/helpers/helpers.dart';
+import 'package:flutter_app/domain/usecases/usecases.dart';
+
+import 'package:flutter_app/data/cache/cache.dart';
+
+class LocalLoadCurrentAccount implements LoadCurrentAccount {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
+
+  Future<AccountEntity> load() async {
+    try {
+      final token = await fetchSecureCacheStorage.fetchSecure('token');
+      return AccountEntity(token);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
+  }
+}

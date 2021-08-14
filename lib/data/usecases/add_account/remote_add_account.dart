4a8d776d23ed51d7ebb9d8b2c713a6ccc591531b
gaiveknopf <avigaive@hotmail.com>
2021-08-14 10:45:05 -0300

    ensure RemoteAddAccount returns an account on 200

--- lib/data/usecases/add_account/remote_add_account.dart
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -1,12 +1,13 @@
-import 'package:flutter_app/domain/helpers/helpers.dart';
 import 'package:meta/meta.dart';
 
-// import '../../../domain/entities/entities.dart';
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
 import '../../../domain/usecases/usecases.dart';
 
 import '../../http/http.dart';
+import '../../../data/models/models.dart';
 
-class RemoteAddAccount {
+class RemoteAddAccount implements AddAccount {
   final HttpClient httpClient;
   final String url;
 
@@ -15,10 +16,11 @@ class RemoteAddAccount {
     @required this.url,
   });
 
-  Future<void> add(AddAccountParams params) async {
+  Future<AccountEntity> add(AddAccountParams params) async {
     final body = RemoteAddAccountParams.fromDomain(params).toJson();
     try {
-      await httpClient.request(url: url, method: 'post', body: body);
+      final httpResponse = await httpClient.request(url: url, method: 'post', body: body);
+      return RemoteAccountModel.fromJson(httpResponse).toEntity();
     } on HttpError catch (error) {
       throw error == HttpError.forbidden ? DomainError.emailInUse : DomainError.unexpected;
     }

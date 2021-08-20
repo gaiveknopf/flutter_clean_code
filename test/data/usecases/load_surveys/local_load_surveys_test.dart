3030db63dd2052bccfa85667156792abae4184e1
gaiveknopf <avigaive@hotmail.com>
2021-08-20 12:46:10 -0300

    commit lesson 73

---
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -0,0 +1,77 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:flutter_app/data/models/models.dart';
+import 'package:flutter_app/domain/entities/entities.dart';
+
+class LocalLoadSurveys {
+  final FetchCacheStorageSpy fetchCacheStorage;
+
+  LocalLoadSurveys({this.fetchCacheStorage});
+
+  Future<List<SurveyEntity>> load() async {
+    final data = await fetchCacheStorage.fetch('surveys');
+    return data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
+  }
+}
+
+class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
+
+abstract class FetchCacheStorage {
+  Future<dynamic> fetch(String key);
+}
+
+void main() {
+  FetchCacheStorageSpy fetchCacheStorage;
+  LocalLoadSurveys sut;
+  List<Map> data;
+
+  List<Map> mockValidData() => [
+        {
+          'id': faker.guid.guid(),
+          'question': faker.randomGenerator.string(10),
+          'date': '2021-07-20T00:00:00Z',
+          'didAnswer': 'false',
+        },
+        {
+          'id': faker.guid.guid(),
+          'question': faker.randomGenerator.string(10),
+          'date': '2021-07-21T00:00:00Z',
+          'didAnswer': 'true',
+        },
+      ];
+
+  void mockFetch(List<Map> list) {
+    data = list;
+    when(fetchCacheStorage.fetch(any)).thenAnswer((_) async => data);
+  }
+
+  setUp(() {
+    fetchCacheStorage = FetchCacheStorageSpy();
+    sut = LocalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
+    mockFetch(mockValidData());
+  });
+
+  test('Should call FetchCacheStorage with correct key', () async {
+    await sut.load();
+    verify(fetchCacheStorage.fetch('surveys')).called(1);
+  });
+
+  test('Should return a list of surveys on success', () async {
+    final surveys = await sut.load();
+
+    expect(surveys, [
+      SurveyEntity(
+          id: data[0]['id'],
+          question: data[0]['question'],
+          dateTime: DateTime.utc(2021, 07, 20),
+          didAnswer: false),
+      SurveyEntity(
+          id: data[1]['id'],
+          question: data[1]['question'],
+          dateTime: DateTime.utc(2021, 07, 21),
+          didAnswer: true),
+    ]);
+  });
+}
